7c4daefa2615c3a98b6b3bccace92ae09e533442
Bon-Jinx <johnkyledevilleres2026@gmail.com>
2026-03-09 21:21:05 +0800

    Delete Player.java

--- Player.java
+++
@@ -1,9 +0,0 @@
-public class Player extends Character {
-    public Player(String name, int maxHp){
-        super(name, maxHp);
-    }
-
-    public int attack(){
-        return Dice.roll(5,10); // random damage 5-10
-    }
-}
